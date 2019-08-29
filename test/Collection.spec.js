07f3e23fcdb05f8b2f5a32aca49339d101708418
Chris Smith <chris@xedi.com>
2019-08-29 12:02:18 +0100

    [master] Added coverage for Collection.eachPromise

--- test/Collection.spec.js
+++ test/Collection.spec.js
@@ -28,6 +28,47 @@ describe('Collection', () => {
     });
 
     describe('eachPromise', () => {
-        it('Should invoke the provided function for each iteration and wait for any promises returned');
+        it('Should throw a TypeError if iterator is not a function', (done) => {
+            let collection = new Collection([ "foo", "bar" ]);
+
+            /*
+                We need to assert that our instance function, when called with a
+                parameter that is not a function, will throw a TypeError.
+
+                `must.throw()` automatically calls our method with a scope of
+                null, so we must bind the scope of our instance and specify any
+                parameters now.
+             */
+            collection.eachPromise.bind(collection, 'a string').must.throw(
+                TypeError,
+                "Invalid Argument: expected Function but got string"
+            );
+
+            done();
+        });
+
+        it('Should invoke the provided function for each iteration and wait for the promises returned', (done) => {
+            let collection = new Collection([ "foo", "bar" ]);
+
+            collection
+                .eachPromise(function () {
+                    return Promise.resolve();
+                })
+                .must.resolve.to.equal(collection);
+
+            done();
+        });
+
+        it('Should invoke the provided function for each iteration and return a resolved promise', (done) => {
+            let collection = new Collection([ "foo", "bar" ]);
+
+            collection
+                .eachPromise(function (item) {
+                    return item;
+                })
+                .must.resolve.to.equal(collection);
+
+            done();
+        });
     });
 });
