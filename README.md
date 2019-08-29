08f63140760b9fca9db0883f19c306be3fca4eb4
Chris Smith <chris@xedi.com>
2019-08-29 13:21:16 +0100

    [master] Added inboxes and inbox(id) functions to the Mailtrap facade

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Mailtrap API Wrapper
+
+[![Actions Status](https://github.com/xedi/libraries-js-mailtrap/workflows/Node%20CI/badge.svg)](https://github.com/xedi/libraries-js-mailtrap/actions)
+[![Dependabot Status](https://api.dependabot.com/badges/status?host=github&identifier=204934407)](https://app.dependabot.com/accounts/xedi/repos/204934407)
+
+## Installation
+
+Install via NPM.
+
+```shell
+npm install --save @xedi/mailtrap
+```
+
+## Usage
+
+```js
+import Mailtrap from 'mailtrap';
+
+// pass in your API Token
+Mailtrap.setApiToken(process.env.MAILTRAP_API_TOKEN);
+
+// You can also set a alternative URL for your Mailtrap service
+// though this has a default of https://mailtrap.io
+Mailtrap.setMailtrapUrl(process.env.MAILTRAP_URL);
+
+// To access an inbox call it from the Mailtrap Facade.
+const inbox = await Mailtrap.inbox(process.env.MY_INBOX_ID);
+
+// You can also list all inboxes attached to the account
+const inboxes = await Mailtrap.inboxes();
+
+```
+---
+
+Made with 🤬 and 🍺 for @Jonny-webedi
--- test/Mailtrap.spec.js
+++ test/Mailtrap.spec.js
@@ -9,76 +9,34 @@ describe('Mailtrap', () => {
         Mailtrap.api_token = undefined;
     });
 
-    describe('getClient', () => {
-        it('Should return a Client instance', (done) => {
-            let client = Mailtrap.getClient();
-
-            client.must.be.an.instanceof(Client);
-
-            done();
-        });
-
-        it('Client instance should have defailt url', (done) => {
-            let client = Mailtrap.getClient();
-
-            client.base_url.must.be.equal('https://mailtrap.io');
-
-            done();
-        });
-
-        it('Client instance should not have a default api token', (done) => {
-            let client = Mailtrap.getClient();
-
-            expect(client.api_token).to.be.undefined();
-
-            done();
-        });
-    });
-
     describe('setMailtrapUrl', () => {
-        it('Should set the Mailtrap URL within the Mailtrap Facade', (done) => {
+        it('Should pass the custom URL through to the client', (done) => {
             Mailtrap.setMailtrapUrl('https://test.test');
 
-            expect(Mailtrap.mailtrap_url).to.be.equal('https://test.test');
-
-            done();
-        });
-
-        it('Should pass the custom URL through to the client', (done) => {
-            let client = Mailtrap.setMailtrapUrl('https://test.test')
-                .getClient();
-
-            client.base_url.must.be.equal('https://test.test');
+            Client.base_url.must.be.equal('https://test.test');
 
             done();
         });
     });
 
     describe('setApiToken', () => {
-        it('Should set the Mailtrap token within the Mailtrap Facade', (done) => {
+        it('Should pass the token through to the client', (done) => {
             Mailtrap.setApiToken('token');
 
-            expect(Mailtrap.api_token).to.be.equal('token');
-
-            done();
-        });
-
-        it('Should pass the token through to the client', (done) => {
-            let client = Mailtrap.setApiToken('token')
-                .getClient();
-
-            client.api_token.must.be.equal('token');
+            Client.api_token.must.be.equal('token');
 
             done();
         });
     });
 
-    describe('clearClient', () => {
-        it('Should remove the cached Client instance from the Mailtrap facade', (done) => {
-            Mailtrap.getClient();
-            Mailtrap.clearClient();
+    describe('resetClient', () => {
+        it('Should reset the Client back to default', (done) => {
+            Mailtrap.setApiToken('token')
+                .setMailtrapUrl('http://test.test')
+                .resetClient();
 
-            expect(Mailtrap.client).to.be.undefined();
+            expect(Client.base_url).must.be.undefined();
+            expect(Client.api_token).must.be.undefined();
 
             done();
         });
